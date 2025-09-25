319e8103aa16d8c6e217833abc5b02bcff21a060
Aamirz23 <mohdaamirali23@gmail.com>
2025-09-25 19:56:48 +0530

    Temperature Converter in Python!
    This can convert Temperature from Celsius to Fahrenheit or Fahrenheit to Celsius by taking input from the user.

---
+++ Temp_Converter.py
@@ -0,0 +1,19 @@
+
+#Formula for celcius to F: (32°F − C) × 5/9 = 0°C
+#Formula for Fahrenheit to Celcius: °C = (°F - 32) × 5/9
+
+conversion = str(input("Enter the type of temperature your want to convert: (for eg: C to F or F to C): "))
+if(conversion == "C to F"):
+    temp1 = float(input("Enter the Temperature in Celcius: "))
+    sol1 = (temp1 * 9/5) + 32
+    print(f"{temp1}°C in fahrenheit is: {sol1}°F")
+
+else:
+    temp2 = float(input("Enter the Temperature in Fahrenheit: "))
+    sol2 = (temp2 - 32) * 5/9
+    print(f"{temp2}°F in Celcius is: {sol2}°C")
+
+
+
+
+ 
